32c1de46d54773b0b07bc8bcda30a3bc64fb74ef
chrisraff <crharfifs@gmail.com>
2019-12-03 18:41:16 -0500

    Trying other classifiers

--- train_classifier.py
+++ train_classifier.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.metrics import classification_report
 
 
@@ -18,9 +21,17 @@ Y=df['labels']
 
 X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2, stratify=Y)
 
-neigh = KNeighborsClassifier(n_neighbors=3)
-neigh.fit(X_train, y_train)
-y_pred = neigh.predict(X_test)
+# clf = KNeighborsClassifier(n_neighbors=1, weights='distance')
+# clf = LogisticRegression()
+clf = RandomForestClassifier(
+    max_depth=5,
+    min_samples_split=10,
+    min_samples_leaf=10,
+    class_weight='balanced'
+    )
+
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
 
 target_names = ['No Evidence', 'Evidence']
 print(classification_report(y_test, y_pred, target_names=target_names))
